8319774f7c1094642ffc69e735bf61993802d755
Anora <muminovaanorka@gmail.com>
2024-10-24 20:55:30 +0300

    abcd

---
+++ abcd.py
@@ -0,0 +1,75 @@
+# print("6-masala")
+# my_tuple =(10,20,30,40,50)
+# my_list=list(my_tuple)
+# my_list.append(60)
+# my_tuple=tuple(my_list)
+# print(my_tuple)
+
+# # 7-task
+# print("7-masala")
+# tuple1=(1,2,3)
+# tuple2=(4,5,6)
+# newtuple=tuple1+tuple2
+# print(newtuple)
+
+# # 1-task
+# print("1-task")
+# numbers=[10,20,30,40,50]
+# numbers.insert(0,100)
+# print(numbers)
+# numbers.append(60)
+# print(numbers)
+
+# # 2-task
+# print("2-task")
+# fruits=["apple","banana","cherry"]
+# fruits.append("orange")
+# fruits.append("kiwi")
+# print(fruits)
+# fruits.insert(1,"mango")
+# fruits.reverse()
+# print(fruits)
+
+# # 3-task
+# print("3-task")
+# students=["Ali","Olim","Zarina","Jasur","Sabrina"]
+# students.index("Sabrina")
+# print(students.index("Sabrina"))
+# print(students[0])
+# print(students[-1])
+
+# # 4-task
+# print("4-task")
+# my_tuple=(5,10,15,20,25)
+# print(my_tuple[2])
+# mylist=list(my_tuple)
+# len(mylist)
+# my_tuple=tuple(mylist)
+# print(my_tuple)
+
+# 5-task
+print("5-task")
+my_tuplee=(1,2,3)
+my_listt=[1,2,3]
+print(my_listt)
+print(my_tuplee)
+
+# 6-task
+print("6-task")
+colors=("red","green","blue")
+l=list(colors)
+l.append("yellow")
+t=tuple(l)
+print(t)
+
+# 7-task
+print("7-task")
+num=[5,10,15,20,25]
+num.append(30)
+num.reverse()
+print(num)
+tup=(10,20,30,40)
+li=list(tup)
+li.reverse()
+ti=tuple(li)
+print(ti)
